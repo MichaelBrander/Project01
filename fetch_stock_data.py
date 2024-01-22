9512ff40b810c24dec1140bec456ade50de3288a
Michael Brander <Michael.Brander@Outlook.com>
2024-01-22 22:36:39 +1100

    Implemented trading hours check and asynchronous data fetching for multiple stocks

--- fetch_stock_data.py
+++ fetch_stock_data.py
@@ -1,7 +1,8 @@
 import aiohttp
 from dotenv import load_dotenv
 import asyncio
-import time
+import pytz
+from datetime import datetime, time
 import os
 import logging
 import json
@@ -27,29 +28,35 @@ async def fetch_stocks(session, api_url):
     except Exception as e:
         logging.error(f"An error occurred: {e}")
 
-async def main(stock_symbol="AAPL", extra_params=None):
-    api_key = os.getenv('API_KEY') 
-    api_url = f"https://financialmodelingprep.com/api/v3/quote/{stock_symbol}?apikey={api_key}"
-    if extra_params:
-        api_url += f"&{extra_params}"
+def is_within_trading_hours():
+    ny_tz = pytz.timezone('America/New_York')
+    ny_time = datetime.now(ny_tz).time()
+    start_time = time(9, 30)
+    end_time = time(16, 0)
+    return start_time <= ny_time <= end_time
 
-    requests_per_second = 2
-    duration_seconds = 23400
-    total_requests = requests_per_second * duration_seconds
-
+async def main(stock_symbols=["AAPL"], extra_params=None):
+    api_key = os.getenv('API_KEY')
     fetched_data = []
 
     async with aiohttp.ClientSession() as session:
-        for _ in range(total_requests):
-            stock_data = await fetch_stocks(session, api_url)
-            if stock_data:
-                fetched_data.append(stock_data)
-            else:
-                logging.info("Failed to fetch data")
-    
-    await asyncio.sleep(0.5)
+        while not is_within_trading_hours():
+            await asyncio.sleep(1)
+
+        while is_within_trading_hours():
+            for symbol in stock_symbols:
+                api_url = f"https://financialmodelingprep.com/api/v3/quote/{symbol}?apikey={api_key}"
+                if extra_params:
+                    api_url += f"&{extra_params}"
+
+                stock_data = await fetch_stocks(session, api_url)
+                if stock_data:
+                    fetched_data.append(stock_data)
+                else:
+                    logging.info(f"Failed to fetch data for {symbol}")
+                await asyncio.sleep(0.1)
 
     with open('stock_data.json', 'w') as file:
         json.dump(fetched_data, file)
 
-asyncio.run(main())
+asyncio.run(main(stock_symbols=["LULU", "CROX", "SHAK", "COST", "WBD", "NVDA", "LMT", "PM", "DJIA"]))
