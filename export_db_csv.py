f714a286160d33ff1ec9bf1708d8a8a9bd5ba442
Michael Brander <Michael.Brander@Outlook.com>
2024-01-23 22:33:55 +1100

    Created script for exporting Project01 database to CSV - script connects to PostgreSQL, executes query to fetch entire table, and writes data to CSV file. Ready for dashboard import

--- export_db_csv.py
+++ export_db_csv.py
@@ -1,27 +1,27 @@
 import psycopg2
 import csv
+import os
+from dotenv import load_dotenv
 
-# Replace these variables with your database connection info
+load_dotenv('/Users/michaelb/Project 01/apikey.env')
+
+password = os.getenv('password')
+
 dbname = "Project01"
 user = "michaelb"
-password = "password"
+password = {password}
 host = "localhost"
 port = "5432"
 
-# SQL query to export data
 query = 'COPY (SELECT * FROM "Project01") TO STDOUT WITH CSV HEADER'
 
-# Path to save the CSV file
 csv_file_path = "/Users/michaelb/Project 01/stock_data_postgres_db.csv"
 
-# Connect to the PostgreSQL database
 conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
 cur = conn.cursor()
 
-# Execute the COPY command and save the output to a file
 with open(csv_file_path, 'w') as csv_file:
     cur.copy_expert(query, csv_file)
 
-# Close the cursor and the connection
 cur.close()
 conn.close()
