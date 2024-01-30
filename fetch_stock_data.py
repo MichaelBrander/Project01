0c5e7f22d68a7bb4dee5cc6f9cc565fca10ed48f
Michael Brander <Michael.Brander@Outlook.com>
2024-01-30 22:32:08 +1100

    Removed asynchronous http requests due to issues when run in Airflow. Removed sleep between requests as no longer necessary due to removal of asyncio and aihttp, rate limits not at risk

--- fetch_stock_data.py
+++ fetch_stock_data.py
@@ -1,8 +1,8 @@
-import aiohttp
+import requests
 from dotenv import load_dotenv
-import asyncio
 import pytz
-from datetime import datetime, time
+from datetime import datetime, time as dt_time
+import time
 import os
 import logging
 import json
@@ -11,19 +11,19 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 load_dotenv('/Users/michaelb/Project 01/apikey.env')
 
-async def fetch_stocks(session, api_url):
+def fetch_stocks(api_url):
     try:
-        async with session.get(api_url) as response:
-            if response.status == 200:
-                data = await response.json()
-                logging.info(f"Data fetched successfully: {data}")
-                return data
-            else:
-                logging.error(f"HTTP Error: {response.status}")
-                return None
-    except aiohttp.ClientConnectionError:
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            data = response.json()
+            logging.info(f"Data fetched successfully: {data}")
+            return data
+        else:
+            logging.error(f"HTTP Error: {response.status_code}")
+            return None
+    except requests.ConnectionError:
         logging.error("Connection Error")
-    except asyncio.TimeoutError:
+    except requests.Timeout:
         logging.error("Request timed out")
     except Exception as e:
         logging.error(f"An error occurred: {e}")
@@ -31,32 +31,32 @@ async def fetch_stocks(session, api_url):
 def is_within_trading_hours():
     ny_tz = pytz.timezone('America/New_York')
     ny_time = datetime.now(ny_tz).time()
-    start_time = time(9, 30)
-    end_time = time(16, 0)
+    start_time = dt_time(0, 30)
+    end_time = dt_time(16, 0)
     return start_time <= ny_time <= end_time
 
-async def main(stock_symbols=["AAPL"], extra_params=None):
+def main(stock_symbols=["AAPL"], extra_params=None):
     api_key = os.getenv('API_KEY')
     fetched_data = []
 
-    async with aiohttp.ClientSession() as session:
-        while not is_within_trading_hours():
-            await asyncio.sleep(1)
+    while not is_within_trading_hours():
+        time.sleep(2.5)
 
-        while is_within_trading_hours():
-            for symbol in stock_symbols:
-                api_url = f"https://financialmodelingprep.com/api/v3/quote/{symbol}?apikey={api_key}"
-                if extra_params:
-                    api_url += f"&{extra_params}"
+    while is_within_trading_hours():
+        for symbol in stock_symbols:
+            api_url = f"https://financialmodelingprep.com/api/v3/quote/{symbol}?apikey={api_key}"
+            if extra_params:
+                api_url += f"&{extra_params}"
 
-                stock_data = await fetch_stocks(session, api_url)
-                if stock_data:
-                    fetched_data.append(stock_data)
-                else:
-                    logging.info(f"Failed to fetch data for {symbol}")
-                await asyncio.sleep(0.1)
-
+            stock_data = fetch_stocks(api_url)
+            if stock_data:
+                fetched_data.append(stock_data)
+            else:
+                logging.info(f"Failed to fetch data for {symbol}")
+            time.sleep(2.5)
+    
     with open('stock_data.json', 'w') as file:
         json.dump(fetched_data, file)
 
-asyncio.run(main(stock_symbols=["LULU", "CROX", "SHAK", "COST", "WBD", "NVDA", "LMT", "PM", "DJIA"]))
+if __name__ == "__main__":
+    main(stock_symbols=["LULU", "CROX", "SHAK", "COST", "WBD", "NVDA", "LMT", "PM", "DJIA"])
